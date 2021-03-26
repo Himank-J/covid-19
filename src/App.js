940f44c69cfb214f2b30dabb63911bc5d41b54de
Himank-J <himankvjain@gmail.com>
2021-03-27 01:02:56 +0530

    Wireframe complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { MenuItem, FormControl, Select } from '@material-ui/core';
+import { MenuItem, FormControl, Select, Card, CardContent } from '@material-ui/core';
+import InfoBox from './components/InfoBox';
+import Map from './components/Map';
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide')
@@ -30,19 +32,39 @@ function App() {
 
   return (
     <div className="app">
-      <div className='app__header'>
-        <h1>COVID-19 TRACKER</h1>
-        <FormControl className='app_dropdown'>
-          <Select variant='outlined' value={country} onChange={onCountryChange}>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-            {
-              countries.map(country => (
-                <MenuItem value={country.value}>{country.name}</MenuItem>
-              ))
-            }
-          </Select>
-        </FormControl>
+      <div className='app__left'>
+        <div className='app__header'>
+          <h1>COVID-19 TRACKER</h1>
+          <FormControl className='app_dropdown'>
+            <Select variant='outlined' value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
+              {
+                countries.map(country => (
+                  <MenuItem value={country.value}>{country.name}</MenuItem>
+                ))
+              }
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className='app__stats'>
+          <InfoBox title='Coronavirus Cases'/>
+          <InfoBox title='Recovered'/>
+          <InfoBox title='Deaths'/>
+        </div>
+
+        <Map />
       </div>
+
+      <Card className='app__right'>
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+
+          <h3>Worldwide new cases</h3>
+        </CardContent>
+      </Card>
+
+
     </div>
   );
 }
